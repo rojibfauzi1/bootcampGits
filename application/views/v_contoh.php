84dad215669abfaa2b271e7c2bd4fdc4b0c704db
rojibfauzi1 <rojibfauzi@gmail.com>
2016-06-09 20:15:14 +0700

    Ini Commit dari cabang

--- application/views/v_contoh.php
+++ application/views/v_contoh.php
@@ -1,2 +1,9 @@
 <?php echo $output ?>
+<<<<<<< Updated upstream
 <H2>Ini contoh git commit 1</H2>
+=======
+<H2>Ini contoh git commit 1</H2>
+<H2>Ini ke dua</H2>
+<h3>Tambah kommit lagi lah</h3>
+<h2>Coba di push</h2>
+>>>>>>> Stashed changes
